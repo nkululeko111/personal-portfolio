740c54adda873a698bd6465bd16a73233e70bff1
nzwane023b <nzwane023b@student.wethinkcode.co.za>
2024-09-05 13:12:52 +0200

    web pages working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,21 @@
-// import { Routes } from '@angular/router';
-import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
 import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'home', pathMatch:'full'},
-    {path: 'home', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'portfolio', component: PortfolioComponent},
-    {path: 'contact', component: ContactComponent},
-    {path: '**', component: HomeComponent}
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, 
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'portfolio', component: PortfolioComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', redirectTo: 'home' } 
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes)], 
     exports: [RouterModule]
 })
-
 export class AppRoutingModule {}
