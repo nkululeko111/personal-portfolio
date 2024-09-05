37c2af9e3a8d0e2fcea43682b47a1832c117fef3
nzwane023b <nzwane023b@student.wethinkcode.co.za>
2024-09-05 10:48:30 +0200

    fixed routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch:'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'portfolio', component: PortfolioComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '**', component: HomeComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
