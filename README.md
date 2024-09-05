ad6a1ef4fc25e248e09f8ece09db8183f7421408
nzwane023b <nzwane023b@student.wethinkcode.co.za>
2024-09-05 10:31:07 +0200

    fixing setup errors

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PersonalPortfolio
+# PersonalProject
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.2.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,13 @@
-import { Component, SimpleChange } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home-page/home.component";
-import { AboutPageComponent } from "./about-page/about-page.component";
-import { PortfolioPageComponent } from "./portfolio-page/portfolio-page.component";
-import { ContactPageComponent } from "./contact-page/contact-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, AboutPageComponent, PortfolioPageComponent, ContactPageComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  ngOnChanges (changes: SimpleChange) {
-
-  
-console.log(changes);
-}
-ngOnInit(){
-
-}
-  
-  title = 'personal-portfolio';
+  title = 'personal-project';
 }
